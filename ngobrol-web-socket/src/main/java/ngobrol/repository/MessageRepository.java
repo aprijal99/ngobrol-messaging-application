826cfff4af9e1a94ac22b5eb3bf933a1d35538e0
Aprijal Ghiyas Setiawan <aprijalghiyas@gmail.com>
2023-05-15 07:09:17 +0700

    Change to h2 database

--- ngobrol-web-socket/src/main/java/ngobrol/repository/MessageRepository.java
+++ ngobrol-web-socket/src/main/java/ngobrol/repository/MessageRepository.java
@@ -9,9 +9,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MessageRepository extends JpaRepository<Message, Integer> {
-    @Query("SELECT m.receiver, COUNT(m.receiver) FROM Message m WHERE m.sender = ?1 GROUP BY m.receiver")
+    @Query("SELECT m.receiver FROM Message m WHERE m.sender = ?1 GROUP BY m.receiver")
     Optional<List<User>> findReceiversBySender(User sender);
-    @Query("SELECT m.sender, COUNT(m.receiver) FROM Message m WHERE m.receiver = ?1 GROUP BY m.sender")
+    @Query("SELECT m.sender FROM Message m WHERE m.receiver = ?1 GROUP BY m.sender")
     Optional<List<User>> findSendersByReceiver(User receiver);
     Optional<List<Message>> findMessagesBySenderAndReceiverOrSenderAndReceiverOrderByCreatedAtDesc(
             User senderAsSender, User receiverAsReceiver, User receiverAsSender, User senderAsReceiver);
